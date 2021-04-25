b438bb04fa41f6c896252f90c30e3d5382daea0e
mashingan <rahmat.d.ruffy@gmail.com>
2021-04-25 16:07:15 +0700

    adjust test with newer patches

--- smapping_test.go
+++ smapping_test.go
@@ -570,20 +570,25 @@ func compareErrorReports(t *testing.T, msgfmt string, msgs, errval, errfield []s
 		)
 		n, err := fmt.Fscanf(reader, msgfmt, &v1, &v2, &v3, &v4, &field)
 		v4 = v4[:len(v4)-1]
-		field = field[:len(field)-1]
-		if n != 5 {
+		if len(field) > 1 {
+			field = field[:len(field)-1]
+		}
+		// The mismatch happened when MapFields which
+		// actually MapTags with tag "" so it will return
+		// the empty string, hence when MapTags it's
+		// scanned 5 // and when MapFields it's scanned 4
+		if !(n == 5 || n == 4) {
 			t.Errorf("Scanned values should 5 but got %d", n)
 			continue
 		}
 		if err != nil {
-			t.Errorf(err.Error())
-			continue
+			t.Log(err.Error())
 		}
 		value := strings.Join([]string{v1, v2, v3, v4}, " ")
 		if notin(value, errval...) {
 			t.Errorf("value '%s' not found", value)
 		}
-		if notin(field, errfield...) {
+		if field != "" && notin(field, errfield...) {
 			t.Errorf("field '%s' not found", field)
 		}
 	}
